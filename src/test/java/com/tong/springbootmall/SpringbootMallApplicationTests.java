579d87bf5d13d7675144cdc0c82c3c0b41591c8a
Fish <sm29729443@gmail.com>
2023-11-20 10:16:05 +0800

    initial springboot project

---
+++ src/test/java/com/tong/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
