a101063e67758b3c97223e18bdb929785f53eede
Fish <sm29729443@gmail.com>
2023-11-21 10:08:35 +0800

    ProductRequest加上@JsonProperty

--- src/main/java/com/tong/springbootmall/dto/ProductRequest.java
+++ src/main/java/com/tong/springbootmall/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.tong.springbootmall.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tong.springbootmall.constants.ProductCategory;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,15 +12,26 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class ProductRequest {
     @NotNull
+    @JsonProperty("product_name")
     private String productName;
+
     @NotNull
+    @JsonProperty("category")
     private ProductCategory category;
+
     @NotNull
+    @JsonProperty("image_url")
     private String imageUrl;
+
     @NotNull
+    @JsonProperty("price")
     private Integer price;
+
     @NotNull
+    @JsonProperty("stock")
     private Integer stock;
+
+    @JsonProperty("description")
     private String description;
 
 
